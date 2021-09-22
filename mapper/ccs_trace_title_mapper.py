55b356be06f7fa53d5daf629e11c7faf51fb69d7
彭珂 <pengk@53kf.com>
2021-09-22 10:42:13 +0800

    数据结构

--- mapper/ccs_trace_title_mapper.py
+++ mapper/ccs_trace_title_mapper.py
@@ -13,7 +13,8 @@ class CcsTraceTitleMapper(object):
     S_COUNT_BY_COMID_AND_TRACE = "SELECT COUNT(*) as `count` FROM `ccs_trace_title` WHERE company_id = :company_id AND trace LIKE CONCAT('%', :trace, '%')"
     S_CCS_TRACE_TITLE_BY_COMPANY_ID = "SELECT title FROM `ccs_trace_title` WHERE company_id = :company_id"
 
-    I_CCS_TRACE_TITLE = "INSERT INTO `ccs_trace_title` (`id`, `trace`, `title`, `description`, `keywords`, `company_id`, `update_time`) VALUES (NULL, :trace, :title, :description, :keywords, :company_id, NOW())"
+    # I_CCS_TRACE_TITLE = "INSERT INTO `ccs_trace_title` (`id`, `trace`, `title`, `description`, `keywords`, `company_id`, `update_time`) VALUES (NULL, :trace, :title, :description, :keywords, :company_id, NOW())"
 
+    I_CCS_TRACE_TITLE = "INSERT INTO `ccs_trace_title` (`id`, `trace`, `title`, `company_id`, `update_time`) VALUES (NULL, :trace, :title, :company_id, NOW())"
 
 
